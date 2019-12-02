dbb9eb8dfac0d564a78e0301cc7559ab1ab3492d
syamd <syamd@LAPTOP-4JB1RCQQ>
2019-12-02 20:48:14 +0530

    02/12/2019 changes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,11 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   serversId=10;
-  serversStatus=true;
+  serversStatus=false;
 
-  constructor() { }
+  serversStatusUpdate='not yet created';
+  serverName='';
 
+  constructor() {
+    setTimeout(()=>{
+      this.serversStatus=true;
+    },2000)
+
+  }
+
+  getServerStatus(){
+    return this.serversStatus;
+  }
   ngOnInit() {
   }
-  
+
+  onCreateServer(){
+    this.serversStatusUpdate='Created';
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName=(<HTMLInputElement>event.target).value;
+  }
 }
